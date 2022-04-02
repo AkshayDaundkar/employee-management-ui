d8d623ba64c7fd9e11cd4ca2bc8d765681a9d233
Akshay Daundkar <akshaydaundkar01@gmail.com>
2022-04-02 15:35:00 +0530

    InitialCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Addemployee from './components/Addemployee';
+import EmployeeList from './components/EmployeeList';
+import UpdateEmployee from './components/UpdateEmployee';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  <BrowserRouter>
+    <Navbar/>
+
+    <Routes>
+          <Route index element={<EmployeeList />} />
+          <Route path="/" element={<EmployeeList />}></Route>
+          <Route path="/employeeList" element={<EmployeeList />} />
+          <Route path="/addEmployee" element={<Addemployee />} />
+          <Route path="/editEmployee/:id" element={<UpdateEmployee />} />
+      </Routes>
+  </BrowserRouter>
+  </>
+  )
 }
 
 export default App;
